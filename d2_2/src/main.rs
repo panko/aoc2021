aebc4f3087fee0d6df8c0bea86b2e93efe8e10ce
Peter Panko <prike18@gmail.com>
2021-12-03 20:10:34 +0100

    use match instead of ifs, this match is cool

--- d2_2/src/main.rs
+++ d2_2/src/main.rs
@@ -2,22 +2,20 @@ fn main() {
     let mut distance = 0;
     let mut depth = 0;
     let mut aim = 0;
+
     let data = std::fs::read_to_string("in.txt").expect("couldn't load file.");
     let vec: Vec<&str> = data.lines().collect();
+
     for elem in vec {
         let mut iterator = elem.split_whitespace();
         let direction = iterator.next().unwrap();
         let value: i32 = iterator.next().unwrap().parse().unwrap();
 
-        if direction == "down" {
-            aim += value;
-        }
-        else if direction == "up" {
-            aim -= value;
-        }
-        else if direction == "forward" {
-            distance += value;
-            depth += aim * value;
+        match direction {
+            "down" => aim += value,
+            "up" => aim -= value,
+            "forward" => {distance += value; depth += aim * value}
+            _ => panic!("unexpected direction")
         }
     }
     println!("{}", distance * depth )
